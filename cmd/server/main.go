d77d4040ce558cc4e1a729db4d793cd9b5c8c988
Almaz Fedorov <fedoro3valmaz@gmail.com>
2026-04-02 00:57:27 +0300

    POST order-parts

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,14 +23,69 @@ type Part struct {
 	Price    float64 `json:"price"`
 }
 
-type Order struct {
-	ID                 int       `json:"id"`
-	ClientName         string    `json:"client_name"`
-	PhoneNumber        string    `json:"phone_number"`
-	DeviceModel        string    `json:"device_model"`
-	ProblemDescription string    `json:"problem_description"`
-	Status             string    `json:"status"`
-	CreationDate       time.Time `json:"creation_date"`
+type RepairOrder struct {
+	ID                 int    `json:"id"`
+	ClientName         string `json:"client_name"`
+	PhoneNumber        string `json:"phone_number"`
+	DeviceModel        string `json:"device_model"`
+	ProblemDescription string `json:"problem_description"`
+	Status             string `json:"status"`
+}
+
+type RepairOrderPartInput struct {
+	PartID   int `json:"part_id"`
+	Quantity int `json:"quantity"`
+}
+
+type RepairOrderInput struct {
+	ClientName         string                 `json:"client_name"`
+	PhoneNumber        string                 `json:"phone_number"`
+	DeviceModel        string                 `json:"device_model"`
+	ProblemDescription string                 `json:"problem_description"`
+	Status             string                 `json:"status"`
+	Parts              []RepairOrderPartInput `json:"items"`
+}
+
+type AddPartToOrderInput struct {
+	OrderID  int `json:"order_id"`
+	PartID   int `json:"part_id"`
+	Quantity int `json:"quantity"`
+}
+
+func (app *application) addPartToOrderHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var input AddPartToOrderInput
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	tx, err := app.db.Begin()
+	if err != nil {
+		http.Error(w, "Failed to start transaction", http.StatusInternalServerError)
+		log.Printf("DB error: %v", err)
+		return
+	}
+	defer tx.Rollback()
+	insertQuery := `INSERT INTO repair_order_part (order_id, part_id, quantity) VALUES ($1, $2, $3)`
+	_, err = tx.Exec(insertQuery, input.OrderID, input.PartID, input.Quantity)
+	if err != nil {
+		http.Error(w, "Failed to insert into repair_order_part", http.StatusInternalServerError)
+		log.Printf("DB error: %v", err)
+		return
+	}
+	updateQuery := `UPDATE part SET quantity = quantity - $1 WHERE id = $2`
+	_, err = tx.Exec(updateQuery, input.Quantity, input.PartID)
+	if err != nil {
+		http.Error(w, "Failed to update repair_order_part", http.StatusInternalServerError)
+		log.Printf("DB error: %v", err)
+		return
+	}
+	tx.Commit()
+	w.WriteHeader(http.StatusOK)
 }
 
 func (app *application) ordersHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,36 +93,36 @@ func (app *application) ordersHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	var input Order
+	var input RepairOrderInput
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	query := `
-	INSERT INTO orders (client_name, phone_number, device_model, problem_description, status) 
+	INSERT INTO repair_order (client_name, phone_number, device_model, problem_description, status) 
 	VALUES ($1, $2, $3, $4, $5) 
 	RETURNING id
 	`
-	var newID int
+	var newOrderID int
 	if input.Status == "" {
 		input.Status = "Принят"
 	}
-	err = app.db.QueryRow(query, input.ClientName, input.PhoneNumber, input.DeviceModel, input.ProblemDescription, input.Status).Scan(&newID)
+	err = app.db.QueryRow(query, input.ClientName, input.PhoneNumber, input.DeviceModel, input.ProblemDescription, input.Status).Scan(&newOrderID)
 	if err != nil {
-		http.Error(w, "Failed to insert into database", http.StatusInternalServerError)
+		http.Error(w, "Failed to insert into repair_order", http.StatusInternalServerError)
 		log.Printf("DB error: %v", err)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "Created order with ID: %d", newID)
+	fmt.Fprintf(w, "Created order with ID: %d", newOrderID)
 }
 
 func (app *application) partsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 
 	case http.MethodGet:
-		rows, err := app.db.Query("SELECT id, name, quantity, price FROM parts")
+		rows, err := app.db.Query("SELECT id, name, quantity, price FROM part")
 		if err != nil {
 			http.Error(w, "Failed to get parts", http.StatusInternalServerError)
 			return
@@ -96,22 +151,21 @@ func (app *application) partsHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		query := `INSERT INTO parts (name, quantity, price) VALUES ($1, $2, $3) RETURNING id`
-		var newID int
-		err = app.db.QueryRow(query, input.Name, input.Quantity, input.Price).Scan(&newID)
+		query := `INSERT INTO part (name, quantity, price) VALUES ($1, $2, $3) RETURNING id`
+		var newOrderID int
+		err = app.db.QueryRow(query, input.Name, input.Quantity, input.Price).Scan(&newOrderID)
 		if err != nil {
 			http.Error(w, "Failed to insert into database", http.StatusInternalServerError)
 			log.Printf("DB error: %v", err)
 			return
 		}
 		w.WriteHeader(http.StatusCreated)
-		fmt.Fprintf(w, "Created part with ID: %d", newID)
+		fmt.Fprintf(w, "Created part with ID: %d", newOrderID)
 
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
 }
 
 func main() {
@@ -149,6 +203,7 @@ func main() {
 
 	http.HandleFunc("/parts", app.partsHandler)
 	http.HandleFunc("/orders", app.ordersHandler)
+	http.HandleFunc("/order-parts", app.addPartToOrderHandler)
 
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE parts (
+CREATE TABLE part (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT,
     quantity BIGINT,
     price DECIMAL(19,4)
 );
 
-CREATE TABLE orders (
+CREATE TABLE repair_order (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     client_name TEXT,
     phone_number TEXT,
@@ -13,4 +13,14 @@ CREATE TABLE orders (
     problem_description TEXT,
     status TEXT,
     creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE repair_order_part (
+    order_id BIGINT,
+    part_id BIGINT,
+    quantity BIGINT,
+    price DECIMAL(19,4),
+    PRIMARY KEY (order_id, part_id),
+    CONSTRAINT fk_order FOREIGN KEY(order_id) REFERENCES repair_order(id),
+    CONSTRAINT fk_part FOREIGN KEY(part_id) REFERENCES part(id)
 );
